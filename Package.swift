188d36548f335dbc6e001727d30115bd21522d4f
DexSAFE <dexsafe.github@gmail.com>
2024-07-22 15:54:21 +0300

    init

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
             .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.6.2")),
-            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.1")),
-            .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
+            .package(url: "https://github.com/DexSAFE/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.1")),
+            .package(url: "https://github.com/DexSAFE/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
         ],
         targets: [
             .target(
